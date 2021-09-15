91bb3d8a6b1fef679aad50c1d55d16f23f3840ab
Prin Ritti-traiphop <prinr2481@gmail.com>
2021-09-15 20:11:32 +0700

    1.7 change Or to And

--- src/main/java/se331/lab/rest/repository/EventRepository.java
+++ src/main/java/se331/lab/rest/repository/EventRepository.java
@@ -14,4 +14,5 @@ public interface EventRepository extends JpaRepository<Event,Long> {
     Page<Event> findByTitle(String title, Pageable pageRequest);
     Page<Event> findByTitleContaining(String title, Pageable pageRequesst);
     Page<Event> findByTitleContainingOrDescriptionContaining(String title,String description,Pageable pageRequest);
+    Page<Event> findByTitleContainingAndDescriptionContaining(String title,String description,Pageable pageRequest);
 }
